0f63d5779900567ba396ed092dc95b55149e12c2
sree-86 <sriram3rhyme@gmail.com>
2019-01-24 14:59:47 -0500

    Initial Commit

---
+++ SlotMachine/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GameViewController.swift
+//  SlotMachine
+//
+//  Created by Sreeram Ramakrishnan on 2019-01-24.
+//  Copyright © 2019 Centennial College. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let images = [#imageLiteral(resourceName: "BackButton"),#imageLiteral(resourceName: "crown"),#imageLiteral(resourceName: "bar"),#imageLiteral(resourceName: "seven"),#imageLiteral(resourceName: "cherry"),#imageLiteral(resourceName: "lemon")]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+    }
+
+   
+}
